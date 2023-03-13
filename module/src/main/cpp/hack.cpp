74c98134cbd18cbf7ca7ff665d657af20c28cdd3
SwaggY <74751337+SwaggY699@users.noreply.github.com>
2023-03-13 04:32:46 +0300

    Add files via upload

---
+++ module/src/main/cpp/hack.cpp
@@ -0,0 +1,240 @@
+#include <cstdint>
+#include <cstring>
+#include <cstdio>
+#include <unistd.h>
+#include <dlfcn.h>
+#include <string>
+#include <EGL/egl.h>
+#include <GLES3/gl3.h>
+
+#include "hack.h"
+#include "imgui.h"
+JNIEnv *g_env = nullptr;
+#include "imgui_impl_android.h"
+#include "imgui_impl_opengl3.h"
+#include "KittyMemory/MemoryPatch.h"
+
+#include "main.h"
+
+const char* gamePKG = "com.nobodyshot.POLYWAR";
+
+#define HOOK(ret, func, ...) \
+    ret (*orig##func)(__VA_ARGS__); \
+    ret my##func(__VA_ARGS__)
+
+bool UnlockG;
+
+HOOK(void, Input, void *thiz, void *ex_ab, void *ex_ac){
+    origInput(thiz, ex_ab, ex_ac);
+    DobbyHook((void*)g_env->functions->RegisterNatives, (void*)hook_RegisterNatives, (void **)&old_RegisterNatives);
+    if (init) ImGui_ImplAndroid_HandleInputEvent((AInputEvent *)thiz); 
+    return;
+}
+
+void SetupImGui() {
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO &io = ImGui::GetIO();
+
+    io.IniFilename = g_IniFileName.c_str();
+    io.DisplaySize = ImVec2((float)g_GlWidth, (float)g_GlHeight);
+
+    ImGui_ImplAndroid_Init(nullptr);
+    ImGui_ImplOpenGL3_Init("#version 300 es");
+    ImGui::StyleColorsLight();
+
+    ImFontConfig font_cfg;
+    font_cfg.SizePixels = 22.0f;
+    io.Fonts->AddFontDefault(&font_cfg);
+
+    ImGui::GetStyle().ScaleAllSizes(3.0f);
+}
+//
+EGLBoolean (*old_eglSwapBuffers)(EGLDisplay dpy, EGLSurface surface);
+EGLBoolean hook_eglSwapBuffers(EGLDisplay dpy, EGLSurface surface) {
+    eglQuerySurface(dpy, surface, EGL_WIDTH, &g_GlWidth);
+    eglQuerySurface(dpy, surface, EGL_HEIGHT, &g_GlHeight);
+
+    if (!g_IsSetup) {
+      SetupImGui();
+      g_IsSetup = true;
+    }
+
+    ImGuiIO &io = ImGui::GetIO();
+
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplAndroid_NewFrame(g_GlWidth, g_GlHeight);
+    ImGui::NewFrame();
+
+    //ImGui::ShowDemoWindow();
+    ImGui::Checkbox("Unlock All Guns", &UnlockG);
+    
+    if (UnlockG) {
+        //
+    } else {
+        //
+    }
+    
+    ImGui::EndFrame();
+    ImGui::Render();
+    glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    return old_eglSwapBuffers(dpy, surface);
+}
+
+int isGame(JNIEnv *env, jstring appDataDir) {
+    if (!appDataDir)
+        return 0;
+    g_env = env;
+    const char *app_data_dir = env->GetStringUTFChars(appDataDir, nullptr);
+    int user = 0;
+    static char package_name[256];
+    if (sscanf(app_data_dir, "/data/%*[^/]/%d/%s", &user, package_name) != 2) {
+        if (sscanf(app_data_dir, "/data/%*[^/]/%s", package_name) != 1) {
+            package_name[0] = '\0';
+          //  LOGW(S("fuck cock limited and sniffed... %s"), app_data_dir);
+            return 0;
+        }
+    }
+    if (strcmp(package_name, gamePKG) == 0) {
+      //  LOGI(S("sniffing cock and game: %s"), package_name);
+        game_data_dir = new char[strlen(app_data_dir) + 1];
+        strcpy(game_data_dir, app_data_dir);
+        env->ReleaseStringUTFChars(appDataDir, app_data_dir);
+        return 1;
+    } else {
+        env->ReleaseStringUTFChars(appDataDir, app_data_dir);
+        return 0;
+    }
+}
+
+static int GetAndroidApiLevel() {
+    char prop_value[PROP_VALUE_MAX];
+    __system_property_get("ro.build.version.sdk", prop_value);
+    return atoi(prop_value);
+}
+
+void dlopen_process(const char *name, void *handle) {
+    //LOGD("dlopen: %s", name);
+    if (!il2cpp_handle) {
+        if (strstr(name, "libil2cpp.so")) {
+            il2cpp_handle = handle;
+     //       LOGI(S("Getting new free cock..."));
+        }
+    }
+    if (!unity_handle) {
+        if (strstr(name, "libunity.so")) {
+            unity_handle = handle;
+      //      LOGI(S("Getting new free cock:2..."));
+        }
+    }
+    if (!egl_handle) {
+        if (strstr(name, "libEGL.so")) {
+            egl_handle = handle;
+      //      LOGI(S("Getting new free cock:3..."));
+        }
+    }
+    if (!gl2_handle) {
+        if (strstr(name, "libGLESv2.so")) {
+            gl2_handle = handle;
+        //    LOGI(S("Getting new free cock:4..."));
+        }
+    }
+}
+
+HOOK_DEF(void*, __loader_dlopen, const char *filename, int flags, const void *caller_addr) {
+    void *handle = orig___loader_dlopen(filename, flags, caller_addr);
+    dlopen_process(filename, handle);
+    return handle;
+}
+
+HOOK_DEF(void*, do_dlopen_V24, const char *name, int flags, const void *extinfo,
+         void *caller_addr) {
+    void *handle = orig_do_dlopen_V24(name, flags, extinfo, caller_addr);
+    dlopen_process(name, handle);
+    return handle;
+}
+
+HOOK_DEF(void*, do_dlopen_V19, const char *name, int flags, const void *extinfo) {
+    void *handle = orig_do_dlopen_V19(name, flags, extinfo);
+    dlopen_process(name, handle);
+    return handle;
+}
+
+void *hack_thread(void *arg) {
+	int api_level = GetAndroidApiLevel();
+   if (api_level < 33) {
+    if (api_level >= 33) {
+        void *addr = DobbySymbolResolver(nullptr,
+                                         "__dl__Z9do_dlopenPKciPK17android_dlextinfoPKv");
+        if (addr) {
+             DobbyHook(addr, (void *) new_do_dlopen_V24,
+                      (void **) &orig_do_dlopen_V24);
+        }
+    } else if (api_level >= 26) {
+        void *libdl_handle = dlopen("libdl.so", RTLD_LAZY);
+        void *addr = dlsym(libdl_handle, "__loader_dlopen");
+        LOGI("__loader_dlopen at: %p", addr);
+        DobbyHook(addr, (void *) new___loader_dlopen,
+                  (void **) &orig___loader_dlopen);
+    } else if (api_level >= 24) {
+        void *addr = DobbySymbolResolver(nullptr,
+                                         "__dl__Z9do_dlopenPKciPK17android_dlextinfoPv");
+        if (addr) {
+            LOGI("get cock at: %p", addr);
+            DobbyHook(addr, (void *) new_do_dlopen_V24,
+                      (void **) &orig_do_dlopen_V24);
+        }
+    } else {
+        void *addr = DobbySymbolResolver(nullptr,
+                                         "__dl__Z9do_dlopenPKciPK17android_dlextinfo");
+        if (addr) {
+            LOGI("get cock at: %p", addr);
+            DobbyHook(addr, (void *) new_do_dlopen_V19,
+                      (void **) &orig_do_dlopen_V19);
+        }
+    }
+    while (!il2cpp_handle) {
+        sleep(1);
+    }
+    origin_thread(il2cpp_handle);
+    sleep(5);
+    while (!unity_handle or !gl2_handle) {
+        sleep(1);
+    }
+    
+    auto eglSwapBuffers = dlsym(unity_handle, "eglSwapBuffers");
+    const char *dlsym_error = dlerror();
+    if (dlsym_error)
+    {
+        DobbyHook(eglSwapBuffers, (void *) hook_eglSwapBuffers, (void **) &old_eglSwapBuffers);
+    } else
+    {
+        DobbyHook(eglSwapBuffers, (void *) hook_eglSwapBuffers, (void **) &old_eglSwapBuffers);
+    }
+    void *sym_input = DobbySymbolResolver("/system/lib/libinput.so", "_ZN7android13InputConsumer21initializeMotionEventEPNS_11MotionEventEPKNS_12InputMessageE");
+    if (NULL != sym_input){
+        DobbyHook((void *)sym_input, (void *) myInput, (void **)&origInput);
+    }
+    do {
+        sleep(1);
+    } while (!isLibraryLoaded(OBFUSCATE("libil2cpp.so")));
+    offsets_load();
+    return nullptr;
+    }else{
+    sleep(2);
+    auto addr = (uintptr_t)dlsym(RTLD_NEXT, "eglSwapBuffers");
+    DobbyHook((void*)addr, (void*)hook_eglSwapBuffers, (void**)&old_eglSwapBuffers);
+    void *sym_input = DobbySymbolResolver(("/system/lib/libinput.so"), ("_ZN7android13InputConsumer21initializeMotionEventEPNS_11MotionEventEPKNS_12InputMessageE"));
+    if (NULL != sym_input) {
+        DobbyHook(sym_input,(void*)myInput,(void**)&origInput);
+    }
+    ProcMap il2cppMap;
+    do {
+        il2cppMap = KittyMemory::getLibraryMap(libName);
+        sleep(1);
+    } while (!il2cppMap.isValid());
+    offsets_load();
+    return nullptr;
+    }
+}
---
+++ module/src/main/cpp/main.h
@@ -0,0 +1,135 @@
+#include <pthread.h>
+#include <jni.h>
+#include <string.h>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include<list>
+#include<vector>
+#include<string.h>
+#include<pthread.h>
+#include<thread>
+#include<cstring>
+#include<jni.h>
+#include<unistd.h>
+#include<fstream>
+#include<iostream>
+#include<dlfcn.h>
+#include <algorithm>
+#include <string>
+#include <map>
+
+#include "Obfuscate.h"
+
+#include <openssl/md5.h>
+#include <md5.h>
+
+#include "Utils.h"
+#include "Logger.h"
+#include "KittyMemory/MemoryPatch.h"
+#include "json.hpp"
+#include "autoaddr.h"
+#include "hack.h"
+
+#include <cstring>
+#include <cstdio>
+#include <unistd.h>
+#include <sys/system_properties.h>
+#include <dlfcn.h>
+
+#include "KittyMemory/MemoryPatch.h"
+
+#include <dlfcn.h>
+#include <cstdlib>
+#include <cinttypes>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <fstream>
+
+#include <EGL/egl.h>
+#include <GLES2/gl2.h>
+
+#include "ImGui/imgui.h"
+#include "ImGui/imgui_internal.h"
+
+#include <pthread.h>
+
+#include "zygisk.hpp"
+
+#include <sys/system_properties.h>
+
+#define targetLibName OBFUSCATE("libil2cpp.so")
+
+JavaVM* publicVM;
+JNIEnv* publicEnv;
+
+#include <EGL/egl.h>
+#include <GLES2/gl2.h>
+
+uintptr_t address = 0;
+
+std::string test;
+std::string test2;
+std::string test3;
+void configt() {
+    ifstream reader("/sdcard/config.txt");
+    if (reader.is_open())
+    {
+        reader >> std::hex >> test;
+        reader >> std::hex >> test2;
+        reader >> std::hex >> test3;
+    }
+    reader.close();
+}
+
+int page = 1;
+
+void cmdExecuteSystem(const char* input){
+    printf("%s", input);
+}
+
+#include <stdlib.h>
+
+typedef unsigned int uint;
+
+char* data;
+#define S(data)
+
+static char buf[500];
+static const char* buf1;
+
+std::string f777 = OBFUSCATE("libil2cpp.so");
+const char *libNames = f777.c_str();
+
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+
+std::string gen_random(const int len) {
+    string id;
+    static const char gsfid[] =
+            "0123456789"
+            "abcdefghijklmnopqrstuvwxyz";
+    srand( (unsigned) time(nullptr) * getpid());
+    id.reserve(len);
+    for (int i = 0; i < len; ++i)
+        id += gsfid[rand() % (sizeof(gsfid) - 1)];
+    return id;
+}
+
+bool radarbool;
+
+struct My_Patches {
+    MemoryPatch RadarHack;
+} hexPatches;
+
+void offsets_load() {
+ 
+  hexPatches.RadarHack = MemoryPatch::createWithHex(targetLibName, 0x140B390, OBFUSCATE("01 00 A0 E3 1E FF 2F E1"));
+  hexPatches.RadarHack.Modify();
+  
+pthread_exit(nullptr);
+}
+
