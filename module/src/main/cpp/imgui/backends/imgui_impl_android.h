8c76e36260a46d200dbfdb19fe9920bf6efc539e
SwaggY <74751337+SwaggY699@users.noreply.github.com>
2023-03-17 22:15:13 +0100

    Update imgui_impl_android.h

--- module/src/main/cpp/imgui/backends/imgui_impl_android.h
+++ module/src/main/cpp/imgui/backends/imgui_impl_android.h
@@ -22,7 +22,8 @@
 struct ANativeWindow;
 struct AInputEvent;
 
-IMGUI_IMPL_API bool     ImGui_ImplAndroid_Init(ANativeWindow* window = 0);
-IMGUI_IMPL_API int32_t  ImGui_ImplAndroid_HandleInputEvent(AInputEvent *input_event, ImVec2 screen_scale);
+IMGUI_IMPL_API bool     ImGui_ImplAndroid_Init(ANativeWindow* window);
+IMGUI_IMPL_API int32_t  ImGui_ImplAndroid_HandleInputEvent(AInputEvent* input_event);
 IMGUI_IMPL_API void     ImGui_ImplAndroid_Shutdown();
-IMGUI_IMPL_API void     ImGui_ImplAndroid_NewFrame(int screen_width = 0, int screen_height = 0);
+IMGUI_IMPL_API void     ImGui_ImplAndroid_NewFrame();
+IMGUI_IMPL_API void     ImGui_ImplAndroid_NewFrame(int width, int height);
