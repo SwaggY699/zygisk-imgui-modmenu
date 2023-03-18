314e897c95798ad87f863d00fc0625d3713568ec
SwaggY <74751337+SwaggY699@users.noreply.github.com>
2023-03-18 22:25:19 +0100

    Update main.h

--- module/src/main/cpp/main.h
+++ module/src/main/cpp/main.h
@@ -19,23 +19,18 @@
 #include <algorithm>
 #include <string>
 #include <map>
-
 #include "Obfuscate.h"
-
 #include "Utils.h"
 #include "Logger.h"
 #include "KittyMemory/MemoryPatch.h"
 #include "json.hpp"
 #include "autoaddr.h"
-
 #include <cstring>
 #include <cstdio>
 #include <unistd.h>
 #include <sys/system_properties.h>
 #include <dlfcn.h>
-
 #include "KittyMemory/MemoryPatch.h"
-
 #include <dlfcn.h>
 #include <cstdlib>
 #include <cinttypes>
@@ -43,61 +38,48 @@
 #include <vector>
 #include <sstream>
 #include <fstream>
-
 #include <EGL/egl.h>
 #include <GLES2/gl2.h>
-
 #include "imgui.h"
 #include "imgui_internal.h"
-
 #include <pthread.h>
-
 #include "zygisk.hpp"
-
 #include <sys/system_properties.h>
-
 #define targetLibName OBFUSCATE("libil2cpp.so")
-
 JavaVM* publicVM;
 JNIEnv* publicEnv;
-
 #include <EGL/egl.h>
 #include <GLES2/gl2.h>
-
 uintptr_t address = 0;
-
 int page = 1;
-
 void cmdExecuteSystem(const char* input){
     printf("%s", input);
 }
-
 #include <stdlib.h>
-
 typedef unsigned int uint;
-
 char* data;
 #define S(data)
-
 static char buf[500];
 static const char* buf1;
-
 std::string f777 = OBFUSCATE("libil2cpp.so");
 const char *libNames = f777.c_str();
-
 #include <iostream>
 #include <iomanip>
 #include <sstream>
+
+#include "autohelp.h"
+#include "Rect.h"
+#include "StrEnc.h"
+
 
 struct My_Patches {
     MemoryPatch UnG;
 } hexPatches;
 
+
 void offsets_load() {
   
   hexPatches.UnG = MemoryPatch::createWithHex(targetLibName, 0x140B390, OBFUSCATE("01 00 A0 E3 1E FF 2F E1"));
   
-  
 pthread_exit(nullptr);
 }
-
