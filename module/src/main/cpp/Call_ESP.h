c7beb1f625d9e024f0b5d0b2865aacc141ca3f07
SwaggY <74751337+SwaggY699@users.noreply.github.com>
2023-03-19 11:01:11 +0100

    Update Call_ESP.h

--- module/src/main/cpp/Call_ESP.h
+++ module/src/main/cpp/Call_ESP.h
@@ -1,23 +1,3 @@
-template <typename T>
-struct monoList {
-    void *unk0;
-    void *unk1;
-    monoArray<T> *items;
-    int size;
-    int version;
-    
-    T getItems(){
-        return items->getPointer();
-    }
-    
-    int getSize(){
-        return size;
-    }
-    
-    int getVersion(){
-        return version;
-    }
-};
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 bool Enable_ESP;
 bool PlayerLine;
